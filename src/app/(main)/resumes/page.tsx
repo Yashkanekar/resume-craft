f54bd0057f250aa70e6a94c781faddb5dc08efce
Yash Kanekar <kanekaryash.11@gmail.com>
2025-03-05 17:26:30 +0530

    setting up next-auth

---
+++ src/app/(main)/resumes/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div>Here is all your resumes</div>;
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,3 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github"
+export const { auth, handlers, signIn } = NextAuth({ providers: [Github] });
